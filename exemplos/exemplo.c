c2d0f5608cb83f3d4aee09d038cc75d6e27ab000
Marco Aurélio <91387895+sodmlx2@users.noreply.github.com>
2023-01-09 21:52:25 -0300

    Update exemplo.c
    Exemplo em Pascal.

--- exemplos/exemplo.c
+++ exemplos/exemplo.c
@@ -1,13 +1,12 @@
 /*
+    Exemplo de Código.
 */
 
-10 dsadsa
-20 dadsad
-30 dasdas
-40 dsadsad
-
+10 program = "teste";
+20 
+30 a = (10 + 10);
+40
 50 goto 10
-60 jmp 50
+60 jmp 50;
 70
-
-80
+80 fim;
